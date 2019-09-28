95ebaf9855b04ed4a0b3bd388e06244fe64e6081
Heisenberg <36684964+grdguez@users.noreply.github.com>
2019-09-28 16:20:58 -0400

    Update chime_weather.sh

--- chime_weather.sh
+++ chime_weather.sh
@@ -22,4 +22,4 @@ fi
 
 weather="$(curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${fahrenheit}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }')"
 
-curl -g -X POST "$webhook_url" -H "Content-Type:application/json" --data-binary '{"Content":"@Present MIA1 Weather Forecast > '"$weather"'"}'
+curl -g -X POST "$webhook_url" -H "Content-Type:application/json" --data-binary '{"Content":"@Present Weather Forecast > '"$weather"'"}'
