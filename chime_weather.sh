fbb9a20ebcf9d13873e7c83a358fb13fcf6321b5
Heisenberg <36684964+grdguez@users.noreply.github.com>
2019-10-31 16:41:01 -0400

    Update chime_weather.sh

--- chime_weather.sh
+++ chime_weather.sh
@@ -5,20 +5,21 @@
 
 webhook_url="WEBHOOK URL HERE"
 
-METRIC=1 #Should be 0 or 1; 0 for F, 1 for C
-
-
-if [ -z $1 ]; then
-
-    echo
-
-    echo "USAGE: chime_weather.sh <zipcode>"
-
-    echo
-
-    exit 0;
-
+zipcode="$1"
+zip_num=${#zipcode}
+# Prompt user if zipcode omitted
+if [[ -z $zipcode ]]; then
+	read -p "Enter your zipcode: " zipcode
+elif [[ $zip_num < 5 ]]; then
+	echo -e "Error: Insufficient digits in zipcode \n"
 fi
+
+#if [ -z $1 ]; then
+#    echo
+#    echo "USAGE: ./weather.sh zipcode"
+#    echo
+#    exit 0;
+#fi
 
 weather="$(curl --connect-timeout 30 -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${fahrenheit}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }')"
 
